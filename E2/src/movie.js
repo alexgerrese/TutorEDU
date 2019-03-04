841bad4e39dedcf44ef748b969b83fb1be0aadc9
jaidharosenblatt <jaidha.rosenblatt@gmail.com>
2019-03-03 20:57:18 -0500

    added E2

---
+++ E2/src/movie.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Movie extends Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img src={ this.props.poster } alt={ this.props.name } className="movie-image"/>
+      </div>
+    )
+  }
+}
+
+export default Movie;
