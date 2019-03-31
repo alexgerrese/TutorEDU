092128b8df22b7c5d42cad35eb1a4d931be348b5
jaidharosenblatt <jaidha.rosenblatt@gmail.com>
2019-03-31 04:40:48 -0400

    began connecting backend to react

---
+++ django/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jaidharosenblatt/Documents/Class/CS290/TutorEDU/django/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
