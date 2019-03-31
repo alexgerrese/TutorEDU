6e00f4a373f263c3a0c295b75a321f470306d288
noah-karpel <npk6@duke.edu>
2019-03-30 21:47:46 -0400

    Added back edu capabilities/ email syntax checker

--- REST/VenvDjango/django-drf-react-quickstart/project/leads/models.py
+++ REST/VenvDjango/django-drf-react-quickstart/project/leads/models.py
@@ -3,15 +3,25 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from .validators import validate_school_email
+
 
 from .managers import CustomUserManager
 
 class Lead(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(_('email address'), unique=True, validators= [validate_school_email])
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
-
+    name = models.CharField(max_length=100)
+#    email = models.EmailField(null=True, validators= [validate_school_email])
+    
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    YEAR_IN_SCHOOL_CHOICES = ((FRESHMAN, 'Freshman'),(SOPHOMORE, 'Sophomore'),(JUNIOR, 'Junior'),(SENIOR, 'Senior'),)
+    year_in_school = models.CharField(max_length=2,choices=YEAR_IN_SCHOOL_CHOICES,default=FRESHMAN,)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
