8fafa1099690b9a546eefd8326d18715ec054653
jaidharosenblatt <jaidha.rosenblatt@gmail.com>
2019-03-31 02:09:55 -0400

    added REST backend to djauth

--- djauth/users/views.py
+++ djauth/users/views.py
@@ -1,10 +1,14 @@
 # users/views.py
-from django.urls import reverse_lazy
-from django.views import generic
+# from django.urls import reverse_lazy
+# from django.views import
+# from .forms import CustomUserCreationForm
+from users.models import CustomUser
+from users.serializers import UserSerializer
+from rest_framework import generics
 
-from .forms import CustomUserCreationForm
-
-class SignUp(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+class SignUp(generics.ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    # form_class = CustomUserCreationForm
+    # success_url = reverse_lazy('login')
+    # template_name = 'signup.html'
