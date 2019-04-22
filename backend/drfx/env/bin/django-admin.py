f137993a3e7fc63fa38fc15c730c64cbd3f06537
noah-karpel <npk6@duke.edu>
2019-04-22 16:51:39 -0400

    new virtualenvironment, what do you think?

---
+++ backend/drfx/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/noahkarpel/Desktop/Duke 2021/2018-19 Second/CS 290/TutorEDU/backend/drfx/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
