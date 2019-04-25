27877a46accc34cc546d8695712042b6fc52f4fc
jaidharosenblatt <jaidha.rosenblatt@gmail.com>
2019-04-25 17:00:52 -0400

    fixed user subject and appointments

--- backend/drfx/users/serializers.py
+++ backend/drfx/users/serializers.py
@@ -2,18 +2,6 @@
 from rest_framework import serializers
 from . import models
 # parsing data from django models to json format -- uses rest framework
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    tutor_appointments = serializers.StringRelatedField(many=True)
-    student_appointments = serializers.StringRelatedField(many=True)
-    subjects = serializers.StringRelatedField(many=True)
-
-    class Meta:
-        model = models.CustomUser
-        fields = ('id','email', 'username','name',
-        'password', 'university','bio', 'client_rating',
-        'is_tutor','is_active', 'tutor_rating','hourly_rate','availabilities','image','tutor_appointments','student_appointments','subjects')
-        write_only_fields = ('password',)
-        read_only_fields = ('id',)
 
 class SubjectSerializer(serializers.ModelSerializer):
     subject_appointments = serializers.StringRelatedField(many=True)
@@ -26,3 +14,16 @@ class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Appointment
         fields = ('id', 'tutor', 'student', 'subject', 'additional_comments', 'date', 'location', 'status', 'rating')
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    tutor_appointments = AppointmentSerializer(many=True)
+    student_appointments = AppointmentSerializer(many=True)
+    subjects = SubjectSerializer(many=True)
+
+    class Meta:
+        model = models.CustomUser
+        fields = ('id','email', 'username','name',
+        'password', 'university','bio', 'client_rating',
+        'is_tutor','is_active', 'tutor_rating','hourly_rate','availabilities','image','tutor_appointments','student_appointments','subjects')
+        write_only_fields = ('password',)
+        read_only_fields = ('id',)
