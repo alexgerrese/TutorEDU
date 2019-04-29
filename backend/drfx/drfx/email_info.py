e17268529201d8303f331726b4f6828ade6a6b68
noah-karpel <npk6@duke.edu>
2019-04-29 01:33:46 -0400

    Email fixed

---
+++ backend/drfx/drfx/email_info.py
@@ -0,0 +1,7 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'tutoreduhost@gmail.com'
+EMAIL_HOST_PASSWORD = 'hgjxajktvocgkklh'
+EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- backend/drfx/drfx/settings.py
+++ backend/drfx/drfx/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .email_info import EMAIL_USE_TLS, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_PORT, EMAIL_BACKEND, DEFAULT_FROM_EMAIL
 # import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -149,10 +150,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 SITE_ID = 1
 # django_heroku.settings(locals())
 # TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tutoreduhost@gmail.com'
-EMAIL_HOST_PASSWORD = 'hgjxajktvocgkklh'
-EMAIL_PORT = 587
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_USE_TLS = EMAIL_USE_TLS
+EMAIL_HOST = EMAIL_HOST
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_PORT = EMAIL_PORT
+EMAIL_BACKEND = EMAIL_BACKEND
+DEFAULT_FROM_EMAIL = DEFAULT_FROM_EMAIL
